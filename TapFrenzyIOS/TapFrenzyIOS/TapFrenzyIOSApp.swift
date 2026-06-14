dcf41accb542dc18f62747f7683f43275c3a0fda
lahirutheekshana <lahirutheekshanabandara2002@gmail.com>
2026-06-14 17:12:12 +0530

    Initial Xcode project setup

---
+++ TapFrenzyIOS/TapFrenzyIOS/TapFrenzyIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapFrenzyIOSApp.swift
+//  TapFrenzyIOS
+//
+//  Created by student1 on 2026-06-14.
+//
+
+import SwiftUI
+
+@main
+struct TapFrenzyIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
