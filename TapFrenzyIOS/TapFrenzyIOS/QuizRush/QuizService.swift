5aa8b297a9f0ba2e3834ce29da0e1c7c4dd3cf83
lahirutheekshana <lahirutheekshanabandara2002@gmail.com>
2026-07-03 16:26:52 +0530

    create the QuizService file in QuizRush game

--- TapFrenzyIOS/TapFrenzyIOS/QuizRush/QuizService.swift
+++ TapFrenzyIOS/TapFrenzyIOS/QuizRush/QuizService.swift
@@ -1,10 +1,10 @@
 import Foundation
 
-// API එකෙන් ප්‍රශ්න ගෙන එන Service Layer එක
+
 class QuizService {
     private let apiUrlString = "https://opentdb.com/api.php?amount=10&type=multiple"
     
-    // ප්‍රශ්න Fetch කරන Async function එක
+    
     func fetchTriviaQuestions() async throws -> [Question] {
         guard let url = URL(string: apiUrlString) else {
             throw URLError(.badURL)
